670d7aaca70d4dddd22324f2b62a4efb264b1d2f
awaas <alexandra.waas@hof-university.de>
2023-05-22 18:13:07 +0200

    Added review values and skintype colors

--- src/components/EventBox.js
+++ src/components/EventBox.js
@@ -10,6 +10,22 @@ const EventBox = ({ eventObject }) => {
     }, [eventObject]);
 
 
+    function getSkinTypeColor(skinType) {
+        switch(skinType) {
+            case 'dry':
+                return '#FBEAD1';
+            case 'oily':
+                return '#DFD7F2';
+            case 'combination':
+                return '#F3D1D2';
+            case 'normal':
+                return '#AAC8D6';
+            case 'sensitive':
+                return '#C6E5E7';
+            default:
+                return '#B6C7B9';
+        }
+    }
 
     return <div className="eventBox flexLeft">
         <div className="imageTest"></div>
@@ -20,11 +36,12 @@ const EventBox = ({ eventObject }) => {
                     <p>{resultEvent === null? "place" : resultEvent.place}</p>
                     <p>{resultEvent === null? "date" : new Date(resultEvent.date).toLocaleDateString()}</p>
                 </div>
-                <div><p className="reviewSmall"><span>&#9733;</span> 5</p></div>
+                <div><p className="reviewSmall"><span>&#9733;</span> {resultEvent === null || resultEvent.rating === null? "0" : parseFloat(resultEvent.rating).toFixed(1)}</p></div>
             </div>
             <br/>
             <div className="flexSpaceBetween textRight">
-                <p className="skinTypeTag">{resultEvent === null? "skintype" : resultEvent.skinType}</p>
+                <p className="skinTypeTag" style={{backgroundColor: getSkinTypeColor(resultEvent === null? '' : resultEvent.skinType)}}>
+                    {resultEvent === null? "skintype" : resultEvent.skinType}</p>
                 <h4 className="priceEventBox">{resultEvent === null? "XX" : resultEvent.price}€ <span>/ Person</span></h4>
             </div>
         </div>
