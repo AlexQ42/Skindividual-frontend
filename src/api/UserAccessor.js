701ae8cb017311fc6d675f8a6bd2be93ac85d7e9
awaas <alexandra.waas@hof-university.de>
2023-06-21 15:12:20 +0200

    Changed all awaits to thens

--- src/api/UserAccessor.js
+++ src/api/UserAccessor.js
@@ -2,16 +2,12 @@ import axiosInstance from "./AxiosInstance";
 import authHeader from "./AuthHeader";
 
 export async function getUser() {
-  try {
-    const response = await axiosInstance.get("/users", { headers: authHeader() });
-    return await response.data;
-
-  }
-  catch (error)
-  {
-    console.error(error);
-    return null;
-  }
+  return axiosInstance.get("/users", {headers: authHeader()})
+      .then(
+      (response) => response.data, (error) => {
+        console.error(error);
+        return null;
+      });
 }
 
 export function patchUser(firstname, lastname, email, skinType) {
